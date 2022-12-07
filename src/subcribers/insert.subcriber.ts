3f127531488850c18ab0a8125cc4f36e089ae610
EnzoFilangi <enzo.filangi@efrei.net>
2022-12-07 17:22:25 +0100

    Added beforeUpdate() to insert.subcriber.ts

--- src/subcribers/insert.subcriber.ts
+++ src/subcribers/insert.subcriber.ts
@@ -1,11 +1,17 @@
-import {EntitySubscriberInterface, EventSubscriber, InsertEvent} from "typeorm";
+import {EntitySubscriberInterface, EventSubscriber, InsertEvent, UpdateEvent} from "typeorm";
 import {validateOrReject, ValidationError} from "class-validator";
 
 @EventSubscriber()
 export class InsertSubscriber implements EntitySubscriberInterface {
-    async beforeInsert(event: InsertEvent<any>) {
+    async beforeInsert(event: InsertEvent<object>) {
         await validateOrReject(event.entity).catch((errors: ValidationError[]) => {
             throw errors;
         });
+    }
+
+    async beforeUpdate(event: UpdateEvent<object>) {
+        await validateOrReject(event.entity).catch((errors: ValidationError[]) => {
+            throw errors;
+        })
     }
 }
