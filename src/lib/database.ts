308e258874cb9c69d033b9e3667c268c311a8018
EnzoFilangi <enzo.filangi@efrei.net>
2022-12-07 17:21:39 +0100

    Cleaned the switch between test and prod DB

--- src/lib/database.ts
+++ src/lib/database.ts
@@ -1,10 +1,15 @@
 import {DataSource} from "typeorm"
 import {UserEntity} from "../entity/user.entity";
+import * as dotenv from "dotenv";
 import {InsertSubscriber} from "../subcribers/insert.subcriber";
+
+
+dotenv.config();
+
 
 export const AppDataSource = new DataSource({
     type: "postgres",
-    url: process.env[`TYPEORM_URL`],
+    url: process.env["NODE_ENV"] === 'production' ? process.env[`TYPEORM_URL`] : process.env[`TYPEORM_URL_TEST`],
     database: "test",
     synchronize: true,
     logging: true,
