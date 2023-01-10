6649af015d4514ba882b0321ee3f85e3ab8d4725
EnzoFilangi <enzo.filangi@efrei.net>
2023-01-10 09:08:03 +0100

    Extracted common behavior for insert and update subscribers

--- src/subcribers/insert.subcriber.ts
+++ src/subcribers/insert.subcriber.ts
@@ -1,23 +1,22 @@
-import {EntitySubscriberInterface, EventSubscriber, InsertEvent, UpdateEvent} from "typeorm";
+import {EntitySubscriberInterface, EventSubscriber, InsertEvent, ObjectLiteral, UpdateEvent} from "typeorm";
 import {validateOrReject, ValidationError} from "class-validator";
 
 @EventSubscriber()
 export class InsertSubscriber implements EntitySubscriberInterface {
+
     async beforeInsert(event: InsertEvent<object>) {
-        // Only validate if the entity TypeORM is about to insert does exist
-        // Indeed, for relations, TypeORM will call this with event.entity = undefined which would make the validation fail
-        if (event.entity) {
-            await validateOrReject(event.entity).catch((errors: ValidationError[]) => {
-                throw errors;
-            });
-        }
+        await this.validateEntity(event.entity);
     }
 
     async beforeUpdate(event: UpdateEvent<object>) {
+        await this.validateEntity(event.entity);
+    }
+
+    async validateEntity(entity: ObjectLiteral | undefined){
         // Only validate if the entity TypeORM is about to update does exist
         // Indeed, for relations, TypeORM will call this with event.entity = undefined which would make the validation fail
-        if (event.entity) {
-            await validateOrReject(event.entity).catch((errors: ValidationError[]) => {
+        if (entity) {
+            await validateOrReject(entity).catch((errors: ValidationError[]) => {
                 throw errors;
             })
         }
