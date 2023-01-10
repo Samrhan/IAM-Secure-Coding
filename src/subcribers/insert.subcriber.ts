8b79de84fae613ebf37810bfc57d89803e306f5b
EnzoFilangi <enzo.filangi@efrei.net>
2023-01-10 08:11:46 +0100

    Tested for undefined value before trying to validate in insert.subcriber.ts

--- src/subcribers/insert.subcriber.ts
+++ src/subcribers/insert.subcriber.ts
@@ -4,14 +4,22 @@ import {validateOrReject, ValidationError} from "class-validator";
 @EventSubscriber()
 export class InsertSubscriber implements EntitySubscriberInterface {
     async beforeInsert(event: InsertEvent<object>) {
-        await validateOrReject(event.entity).catch((errors: ValidationError[]) => {
-            throw errors;
-        });
+        // Only validate if the entity TypeORM is about to insert does exist
+        // Indeed, for relations, TypeORM will call this with event.entity = undefined which would make the validation fail
+        if (event.entity) {
+            await validateOrReject(event.entity).catch((errors: ValidationError[]) => {
+                throw errors;
+            });
+        }
     }
 
     async beforeUpdate(event: UpdateEvent<object>) {
-        await validateOrReject(event.entity).catch((errors: ValidationError[]) => {
-            throw errors;
-        })
+        // Only validate if the entity TypeORM is about to update does exist
+        // Indeed, for relations, TypeORM will call this with event.entity = undefined which would make the validation fail
+        if (event.entity) {
+            await validateOrReject(event.entity).catch((errors: ValidationError[]) => {
+                throw errors;
+            })
+        }
     }
 }
